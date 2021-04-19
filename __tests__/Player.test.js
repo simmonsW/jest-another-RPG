f900b7c8a40f2dbb252312bffc2e982faebdb3fc
Wyatt Simmons <simmonsw182@gmail.com>
2021-04-19 14:42:03 -0700

    added player's stats test and player's inventory test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,3 +17,21 @@ test('creates a player object', () => {
     expect.arrayContaining([expect.any(Object)])
   );
 });
+
+test("get player's stats as an object", () => {
+  const player = new Player('Dave');
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player of returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
