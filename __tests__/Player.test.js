7c3fc19a95bb43c82936b70c2ca819b16bae1762
Wyatt Simmons <simmonsw182@gmail.com>
2021-04-19 17:54:05 -0700

    added reduceHealth test and prototype for Player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -51,3 +51,16 @@ test('check if player is alive or not', () => {
 
   expect(player.isAlive()).toBeFalsy();
 });
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
